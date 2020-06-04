a2dc24976cc0eb6edc27271013400c0dd2932be4
kimhan0421 <48988891+kimhan0421@users.noreply.github.com>
2020-06-04 18:43:45 +0900

    Ka)Internship_2019

--- Study_python/programs1.py
+++ Study_python/programs1.py
@@ -1,5 +1,13 @@
 """크레인 인형뽑기 게임"""
 
+board =[[0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 3],
+        [0, 2, 5, 0, 1],
+        [4, 2, 4, 4, 2],
+        [3, 5, 1, 3, 1]
+        ]
+moves = [1,5,3,5,1,2,1,4]
+
 def solution(board, moves):
     stack = [] #장바구니
     result = 0 #터뜨려진 인형 개수
@@ -15,3 +23,4 @@ def solution(board, moves):
                         result += 2
                 break
     return result
+print( solution(board, moves))
