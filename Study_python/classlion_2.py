51ab2958c9a7e82cddc7005d10058b4945076257
kimhan0421 <48988891+kimhan0421@users.noreply.github.com>
2020-06-06 22:55:42 +0900

    Time reduction

--- Study_python/classlion_2.py
+++ Study_python/classlion_2.py
@@ -1,7 +1,7 @@
 """n번째 소수
 소수를 크기 순으로 나열하면 2, 3, 5, 7, 11, 13, ... 과 같이 됩니다.
 이 때 100번째 소수까지 구하시면 됩니다."""
-
+"""
 # is_prime 함수는 특정 숫자(n)이 들어왔을 때 
 # 그 숫자가 소수면 True를 반환하고 아니면 False를 반환하는 함수입니다.
 def is_prime(n):
@@ -11,6 +11,24 @@ def is_prime(n):
                 return False
     else :
         return False
+    return True"""
+
+#시간 줄이기
+def is_prime(n: int) -> bool: #함수에 직접 주석 작성. 콜론으로 매개변수에 할당되야 하는 형식을 할당.
+                                #화살표로 반환값에 할당되어야 하는 형식 할당
+    if n < 2: #소수 성질 이용
+        return False
+    if n in (2, 3): #n이 2라면
+        return True
+    if n % 2 is 0 or n % 3 is 0: #n이 2,3으로 나눠지는 경우
+        return False
+    if n < 9:
+        return True
+    k, l = 5, n**0.5
+    while k <= l:
+        if n % k is 0 or n % (k+2) is 0:
+            return False
+        k += 6
     return True
 
 # prime_number_list 함수는 길이(length)가 들어왔을 때
