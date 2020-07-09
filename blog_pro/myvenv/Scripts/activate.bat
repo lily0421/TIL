295a7bddeea1a2fb9d8846e2967bbc2d1268615a
kimhansol <kimhan0421@naver.com>
2020-07-10 00:39:05 +0900

    update bootstrap

--- blog_pro/myvenv/Scripts/activate.bat
+++ blog_pro/myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\class_lion\blog_pro\myvenv
+set VIRTUAL_ENV=C:\class_lion\TIL\blog_pro\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
